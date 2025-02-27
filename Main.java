ecb3b3516250b50d3f211ea385167b32280e86da
israviei255 <isra.viei255@gmail.com>
2025-02-26 23:06:26 -0300

    arquivo Main modificado

--- Main.java
+++ Main.java
@@ -2,6 +2,7 @@ package m1s03;
 
 public class Main {
     public static void main(String[] args){
-        System.out.print("Hello World!");
+        System.out.println("Hello World!");
+        System.out.print("Hellow World2");
     }
 }
